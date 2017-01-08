d4a387553b835e72c7f06180c2525f7487d704f2
Eric Crosson <esc@ericcrosson.com>
2017-01-07 21:46:15 -0600

    Add package definition

---
+++ darkokai-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "darkokai-theme" "0.1.1" "Dark take on a monokai theme with a version for inverted screens" 'nil :url "https://github.com/sjrmanning/darkokai" :keywords '("color" "theme"))
